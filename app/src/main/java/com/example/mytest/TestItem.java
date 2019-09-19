2a4054e770b8e9a6b42dfa9bc87692d97192104a
ld <514429231@qq.com>
2019-09-20 00:21:44 +0800

    一些知识点汇总

--- app/src/main/java/com/example/mytest/TestItem.java
+++ app/src/main/java/com/example/mytest/TestItem.java
@@ -8,4 +8,28 @@ public class TestItem {
         this.itemWord = itemWord;
         this.itemActivity = itemActivity;
     }
+
+    /*
+    postDelayed(500)的实现原理
+    sharedpreferance是进程不安全的，线程安全的，表现和原因
+    单例的发展史，推荐的三种写法 1、double check 2、静态内部类 3、枚举类
+    单链表的反转处理
+    线程的各个状态是怎样
+    ontouch和onclick的冲突解决
+    */
+
+    /*
+    插件化的原理
+    NDK
+    IM的相关一坨技术
+    SDK开发，怎么打包成SDK
+    */
+
+    /*
+    Socket通信
+    ContentProvider的使用
+    EventBus
+    注解
+    */
+
 }
